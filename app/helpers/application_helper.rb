1b89e5b2159cb4a97857e930ade6fb7cc01561a3
Yusuke Kamihanawa <codingpurpose@gmail.com>
2026-08-25 15:28:07 +0900

    added homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
   def time_based_greeting
-    Time.current.hour
+    current_time = Time.current.hour
+    message = ""
+    if current_time.between?(0, 12)
+      message = "Good Morning"
+    elsif current_time.between?(12, 18)
+      message = "Good Afternoon"
+    else
+      message = "Good Evening"
+    end
+    message
+    # current_time
   end
 end
